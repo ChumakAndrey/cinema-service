fd2c50cc4e684f80301663b2bd38e4b164c59ba7
Andrey Chumak <chumakandrey7@gmail.com>
2022-08-01 14:56:07 +0300

    Changed main package name

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -17,7 +17,7 @@ import cinema.security.AuthenticationService;
 import cinema.service.CinemaHallService;
 
 public class Main {
-    private static Injector injector = Injector.getInstance("mate.academy");
+    private static Injector injector = Injector.getInstance("cinema");
 
     public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
